b9f3c157137c189b32940921ad8188d6dd01f4d8
Романов Александр Владимирович <garlife@gmail.com>
2022-01-27 00:21:56 +0300

    second way of developing

--- src/App.js
+++ src/App.js
@@ -2,44 +2,31 @@ import "./App.css";
 import React from "react";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      count: 0,
-    };
-
-    this.handleIncrement = this.handleIncrement.bind(this);
-    this.handleDecrement = this.handleDecrement.bind(this);
-    this.handleReset = this.handleReset.bind(this);
+  state = {
+    count: 0
   }
 
-  handleIncrement() {
-    this.setState((prevState, props) => ({
-      count: prevState.count + 1
-    }));
-  }
-
-  handleDecrement() {
-    this.setState((prevState, props) => ({
-      count: prevState.count - 1
-    }));
-  }
-
-  handleReset() {
-    this.setState(() => ({
-      count: 0
-    }));
-  }
-
-  render() {
-    return (
-      <div style={{display: "flex", justifyContent: "center", padding: "100px"}}>
-        <button onClick={this.handleIncrement}>increment</button>
-        <button onClick={this.handleDecrement}>decrement</button>
-        <button onClick={this.handleReset}>reset</button>
+  render(){
+    return(
+      <>
+        <button onClick={() => this.handleIncrement()}>Increment</button>
+        <button onClick={() => this.handleDecrement()}>Decrement</button>
+        <button onClick={() => this.handleReser()}>Reset</button>
         <h3>{this.state.count}</h3>
-      </div>
-    );
+      </>
+    )
+  }
+
+  handleIncrement(){
+    this.setState((prevState) => ({count: prevState.count + 1}))
+  }
+
+  handleDecrement(){
+    this.setState((prevState) => ({count: prevState.count - 1}))
+  }
+
+  handleReser(){
+    this.setState((prevState) => ({count: 0}))
   }
 }
 
